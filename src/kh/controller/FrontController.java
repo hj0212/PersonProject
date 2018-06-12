3982c6f84ed1db67223bff5c4460f6f95f828a7d
HJ Kwon <hyejin0212@gmail.com>
2018-06-12 16:23:44 +0900

    FrontController 수정 중

--- src/kh/controller/FrontController.java
+++ src/kh/controller/FrontController.java
@@ -12,29 +12,23 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet("/FrontController")
 public class FrontController extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public FrontController() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String requestURI = request.getRequestURI();
+		String contextPath = request.getContextPath();
+		String command = requestURI.substring(contextPath.length());
+		
+		
+		if(command.equals("input.do")) {
+			
+			
+		} else if(command.equals("output.do")) {
+			
+			
+		}
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
